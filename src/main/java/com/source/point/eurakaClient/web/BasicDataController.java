63f58ff2c4c631c1ce8bbae7d7fe6308d5b128ec
xitiaoyu <xitiaoyu@evergrande.com>
2019-12-16 17:57:43 +0800

    init

---
+++ src/main/java/com/source/point/eurakaClient/web/BasicDataController.java
@@ -0,0 +1,19 @@
+package com.source.point.eurakaClient.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping("/api")
+@Api(description = "基础数据相关", tags = "BASIC-DATA")
+public class BasicDataController {
+
+	@GetMapping("/query")
+	public String query() {
+		return "好的--->"+System.currentTimeMillis();
+	}
+
+}
---
+++ src/test/java/com/source/point/eurakaClient/EurakaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.source.point.eurakaClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
